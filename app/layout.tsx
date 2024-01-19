27537e97bc0c6410997ec07350cd9377a80434f5
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-19 00:26:39 -0300

    style(body): add desktop layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body
-        className={`${redHatDisplay.className} bg-mobile flex h-screen w-screen items-center justify-center bg-PaleBlue bg-top bg-no-repeat`}
+        className={`${redHatDisplay.className} flex h-screen w-screen items-center justify-center bg-PaleBlue bg-mobile bg-top bg-no-repeat lg:h-auto lg:bg-desktop lg:bg-cover`}
       >
         {children}
       </body>
